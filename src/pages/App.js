5a345f76e191b8aada96145f4dfe71c2afb8bf97
Tasmiha Hassan <htasmiha@gmail.com>
2020-10-08 01:46:55 -0400

    Fixing deployment

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Navbar from "../components/Navbar.js";
 import Footer from "../components/Footer.js";
@@ -7,17 +7,28 @@ import About from "../components/About.js";
 import Projects from "../components/Projects.js";
 import Contact from "../components/Contact.js";
 import Timeline from "../components/Timeline.js";
-import AOS from 'aos';
+// import AOS fronm 'aos';
 import 'aos/dist/aos.css'
 import "../styles/global.css"
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function App() {
-  AOS.init({
-    delay: 200,
-    duration: 1200,
-    once: false
-  })    
+
+  useEffect(() => {
+    const isBrowser = typeof window !== "undefined";
+    const AOS = isBrowser ? require("aos") : undefined;
+    AOS.init({
+      delay: 200,
+      duration: 1200,
+      once: false
+    })   
+    AOS.refresh()
+  });
+  // AOS.init({
+  //   delay: 200,
+  //   duration: 1200,
+  //   once: false
+  // })    
   
   return (
     <>
