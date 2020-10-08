87afd922abfad1edb474cadc1e02f6cdda892557
Tasmiha Hassan <htasmiha@gmail.com>
2020-10-08 01:45:35 -0400

    Updates

---
+++ component---src-pages-index-js-a00db76668278d1ce94b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return r}));var u=t("q1tI"),a=t.n(u),c=t("3moH");function r(){return a.a.createElement("div",null,a.a.createElement(c.default,null))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-a00db76668278d1ce94b.js.map
